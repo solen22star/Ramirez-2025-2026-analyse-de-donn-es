8b5a44c07b273ac05d760837472e4b124d53e34a
solen22star <87xcbnqywd@privaterelay.appleid.com>
2025-12-19 00:45:29 +0100

    Add files via upload

---
+++ Seance-01/src/main.py
@@ -0,0 +1,8 @@
+print('Bienvenue dans le cours d\'analyse de données en géographie !')
+
+import numpy
+import pandas as pd
+import geopandas as gd
+
+data = pd.DataFrame({'A': [1, 2, 3]})
+print(data)
